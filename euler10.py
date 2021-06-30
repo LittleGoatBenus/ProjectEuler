210bb1f01f57209ef423c15f7ce23ede75a84f26
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2021-06-30 17:58:31 +0200

    Create euler10.py

---
+++ euler10.py
@@ -0,0 +1,24 @@
+import math
+import time
+def is_prime(n):
+    if n <= 1:
+        return False
+ 
+    max_div = math.floor(math.sqrt(n))
+    for i in range(2, 1 + max_div):
+        if n % i == 0:
+            return False
+    return True
+ 
+
+total = 0
+c = 0 #for counting
+ 
+for n in range(1,2000000):
+    if is_prime(n) == True:
+        total += n
+   
+   
+   
+
+print(total)
